61c7ce4b820d82d43293e068c9dca827728336f7
Billz <58287516+billz94@users.noreply.github.com>
2021-11-22 10:38:10 +0200

    Update edrs_setup.sh
    updating script

--- edrs_setup.sh
+++ edrs_setup.sh
@@ -1,4 +1,17 @@
 #!/bin/bash --login
+
+echo "____________________________________________"
+echo "Checking out to Current Folder"
+echo "____________________________________________"
+git checkout .
+echo "____________________________________________"
+echo "Cleaning Git Cache"
+echo "____________________________________________"
+git clean -f
+echo "____________________________________________"
+echo "Checking out to no Couch-db Application"
+echo "____________________________________________"
+git checkout couchdb-removed
 echo "____________________________________________"
 echo "Removing Gemfile.lock"
 echo "____________________________________________"
@@ -8,7 +21,7 @@ echo "Installing Local Gems"
 echo "____________________________________________"
 bundle install --local
 echo "____________________________________________"
-echo "running bin_update art"
+echo "Setting up edrs application"
 echo "____________________________________________"
 bundle exec rake edrs:setup
 echo "--------------------------------------------"
